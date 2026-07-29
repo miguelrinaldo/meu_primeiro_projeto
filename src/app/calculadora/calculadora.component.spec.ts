799d8e0731eb3fbcf1e887e8c91f8b84567b26f9
Miguel Rinaldo <miguel.r.nascimento9@aluno.senai.br>
2026-07-28 21:20:13 -0300

    Primeiro projeto Angular

--- src/app/calculadora/calculadora.component.spec.ts
+++ src/app/calculadora/calculadora.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CalculadoraComponent } from './calculadoraComponent';
+import { Calculadora } from './calculadoraComponent';
 
 describe('Calculadora', () => {
-  let component: CalculadoraComponent;
-  let fixture: ComponentFixture<CalculadoraComponent>;
+  let component: Calculadora;
+  let fixture: ComponentFixture<Calculadora>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CalculadoraComponent],
+      imports: [Calculadora],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(CalculadoraComponent);
+    fixture = TestBed.createComponent(Calculadora);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
