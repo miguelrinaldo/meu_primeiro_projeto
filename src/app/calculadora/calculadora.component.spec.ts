1988df978a64f66a0d09526a3c41b17ae572e1b0
Miguel Rinaldo <miguel.r.nascimento9@aluno.senai.br>
2026-08-03 20:55:00 -0300

    meu primeiro projeto angular

--- src/app/calculadora/calculadora.component.spec.ts
+++ src/app/calculadora/calculadora.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Calculadora } from './calculadoraComponent';
+import { CalculadoraComponent } from './calculadoraComponent';
 
 describe('Calculadora', () => {
-  let component: Calculadora;
-  let fixture: ComponentFixture<Calculadora>;
+  let component: CalculadoraComponent;
+  let fixture: ComponentFixture<CalculadoraComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Calculadora],
+      imports: [CalculadoraComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Calculadora);
+    fixture = TestBed.createComponent(CalculadoraComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
--- src/app/calculadora/calculadoraComponent.ts
+++ src/app/calculadora/calculadoraComponent.ts
@@ -13,8 +13,30 @@ import { FormsModule } from '@angular/forms'
   })
   
   
-export class Calculadora {
+export class CalculadoraComponent {
   numero1 = 0;
   numero2 = 0;
   resultado = 0;
+
+  somar(){
+  this.resultado =
+  this.numero1 + this.numero2;
 }
+subtrair(){
+  this.resultado =
+  this.numero1 - this.numero2;
+}
+
+multiplicar(){
+  this.resultado =
+  this.numero1 * this.numero2;
+}
+
+dividir(){
+  this.resultado =
+  this.numero1 / this.numero2
+}
+
+
+}
+
